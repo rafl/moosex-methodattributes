baea355ef6126bc94ff2c4feab93a69846d34ca7
Florian Ragwitz <rafl@debian.org>
2009-02-12 14:29:30 +0100

    Add a meta class role to store attributes keyed by the address of the CVs.

---
+++ lib/MooseX/MethodAttributes/Role/Meta/Class.pm
@@ -0,0 +1,15 @@
+package MooseX::MethodAttributes::Role::Meta::Class;
+
+use Moose::Role;
+use MooseX::Types::Moose qw/HashRef/;
+
+use namespace::clean -except => 'meta';
+
+has attributes_for => (
+    is => 'ro',
+    isa => HashRef,
+    lazy => 1,
+    default => sub { +{} },
+);
+
+1;
