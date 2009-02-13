81f215fb2b608fc347786e52fff481e4ef83f014
Florian Ragwitz <rafl@debian.org>
2009-02-13 21:38:31 +0100

    Docs for MooseX::MethodAttributes::Role::Meta::Class.

--- lib/MooseX/MethodAttributes/Role/Meta/Class.pm
+++ lib/MooseX/MethodAttributes/Role/Meta/Class.pm
@@ -1,9 +1,21 @@
 package MooseX::MethodAttributes::Role::Meta::Class;
+# ABSTRACT: metaclass role for storing code attributes
 
 use Moose::Role;
 use MooseX::Types::Moose qw/HashRef/;
 
 use namespace::clean -except => 'meta';
+
+=attr _attributes_for
+
+Stores a HashRef holding the attributes for methods that already have been
+parsed. Keyed by the reference address of the methods coderefs.
+
+Entries in that hash get removed by
+C<MooseX::MethodAttributes::Role::Meta::Method> when constructing the
+C<attributes> attribute.
+
+=cut
 
 has _attributes_for => (
     is => 'ro',
