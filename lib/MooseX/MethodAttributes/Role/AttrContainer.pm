a3d1b03782fcaa1180691275e29416660a62aa2a
Florian Ragwitz <rafl@debian.org>
2009-02-13 22:17:57 +0100

    Docs for MooseX::MethodAttributes::Role::AttrContainer.

--- lib/MooseX/MethodAttributes/Role/AttrContainer.pm
+++ lib/MooseX/MethodAttributes/Role/AttrContainer.pm
@@ -6,6 +6,14 @@ use Moose::Util qw/find_meta/;
 
 use namespace::clean -except => 'meta';
 
+=method MODIFY_CODE_ATTRIBUTES ($code, @attrs)
+
+Accepts a list of attributes for a coderef and stores it the class' metaclass.
+
+See L<attributes>.
+
+=cut
+
 sub MODIFY_CODE_ATTRIBUTES {
     my ($class, $code, @attrs) = @_;
     find_meta($class)->register_method_attributes($code, \@attrs);
