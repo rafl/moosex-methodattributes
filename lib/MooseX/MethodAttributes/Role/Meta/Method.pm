cfe1feca081416568a2ccad4ca571c241c4d1c40
Florian Ragwitz <rafl@debian.org>
2009-02-13 21:52:10 +0100

    Docs for MooseX::MethodAttributes::Role::Meta::Method.

--- lib/MooseX/MethodAttributes/Role/Meta/Method.pm
+++ lib/MooseX/MethodAttributes/Role/Meta/Method.pm
@@ -1,14 +1,28 @@
 package MooseX::MethodAttributes::Role::Meta::Method;
+# ABSTRACT: metamethod role allowing code attribute introspection
 
 use Moose::Role;
 
 use namespace::clean -except => 'meta';
+
+=attr attributes
+
+Gets the list of code attributes of the method represented by this meta method.
+
+=cut
 
 has attributes => (
     is      => 'ro',
     lazy    => 1,
     builder => '_build_attributes',
 );
+
+=method _build_attributes
+
+Builds the value of the C<attributes> attribute based on the attributes
+captured in the associated meta class.
+
+=cut
 
 sub _build_attributes {
     my ($self) = @_;
