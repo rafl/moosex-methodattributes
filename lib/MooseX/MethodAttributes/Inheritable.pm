cea2a50fae545d39e3692af6f2300c19dc91db34
Florian Ragwitz <rafl@debian.org>
2009-02-14 05:49:42 +0100

    Docs for MooseX::MethodAttributes::Inheritable.

--- lib/MooseX/MethodAttributes/Inheritable.pm
+++ lib/MooseX/MethodAttributes/Inheritable.pm
@@ -1,4 +1,25 @@
 package MooseX::MethodAttributes::Inheritable;
+# ABSTRACT: inheritable code attribute introspection
+
+=head1 SYNOPSIS
+
+    package BaseClass;
+    use base qw/MooseX::MethodAttributes::Inheritable/;
+
+    package SubClass;
+    use base qw/SubClass/;
+
+    sub foo : Bar {}
+
+    my $attrs = SubClass->meta->get_method('foo')->attributes; # ["Bar"]
+
+=head1 DESCRIPTION
+
+This module does the same as C<MooseX::MethodAttributes>, except that classes
+inheriting from other classes using it don't need to do anything special to get
+their code attributes captured.
+
+=cut
 
 use Moose;
 
