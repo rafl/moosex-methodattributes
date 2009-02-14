67f84c8d50e3f6556b0b650199bab4ee340708e3
Florian Ragwitz <rafl@debian.org>
2009-02-14 05:57:59 +0100

    Docs for MooseX::MethodAttributes::Role::Meta::Method::Wrapped.

--- lib/MooseX/MethodAttributes/Role/Meta/Method/Wrapped.pm
+++ lib/MooseX/MethodAttributes/Role/Meta/Method/Wrapped.pm
@@ -1,8 +1,15 @@
 package MooseX::MethodAttributes::Role::Meta::Method::Wrapped;
+# ABSTRACT: wrapped metamethod role allowing code attribute introspection
 
 use Moose::Role;
 
 use namespace::clean -except => 'meta';
+
+=method attributes
+
+Gets the list of code attributes of the original method this meta method wraps.
+
+=cut
 
 sub attributes {
     my ($self) = @_;
