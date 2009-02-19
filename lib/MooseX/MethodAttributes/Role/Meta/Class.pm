78ecf31a426aba1fdc07504cb6eb8b4de1047c0e
Florian Ragwitz <rafl@debian.org>
2009-02-19 07:52:28 +0100

    Add and use a _method_attribute_list attribute to maintain the order of registered attribute methods.

--- lib/MooseX/MethodAttributes/Role/Meta/Class.pm
+++ lib/MooseX/MethodAttributes/Role/Meta/Class.pm
@@ -2,15 +2,22 @@ package MooseX::MethodAttributes::Role::Meta::Class;
 # ABSTRACT: metaclass role for storing code attributes
 
 use Moose::Role;
-use MooseX::Types::Moose qw/HashRef/;
+use MooseX::Types::Moose qw/HashRef ArrayRef Str Int/;
 
 use namespace::clean -except => 'meta';
 
 has _method_attribute_map => (
-    is => 'ro',
-    isa => HashRef,
-    lazy => 1,
-    default => sub { +{} },
+    is        => 'ro',
+    isa       => HashRef[ArrayRef[Str]],
+    lazy      => 1,
+    default   => sub { +{} },
+);
+
+has _method_attribute_list => (
+    is      => 'ro',
+    isa     => ArrayRef[Int],
+    lazy    => 1,
+    default => sub { [] },
 );
 
 =method register_method_attributes ($code, $attrs)
@@ -21,6 +28,7 @@ Register a list of attributes for a code reference.
 
 sub register_method_attributes {
     my ($self, $code, $attrs) = @_;
+    push @{ $self->_method_attribute_list }, 0 + $code;
     $self->_method_attribute_map->{ 0 + $code } = $attrs;
     return;
 }
