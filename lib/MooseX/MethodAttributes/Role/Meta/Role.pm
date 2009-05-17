fc2db43096d9f1a53e4a425f4a2042082f1953d0
t0m <bobtfish@bobtfish.net>
2009-05-17 03:19:21 +0100

    Make the tests pass when role is composed onto a class, woo - this may actually be enough to make Catalyst work..

--- lib/MooseX/MethodAttributes/Role/Meta/Role.pm
+++ lib/MooseX/MethodAttributes/Role/Meta/Role.pm
@@ -1,6 +1,10 @@
 package MooseX::MethodAttributes::Role::Meta::Role;
 # ABSTRACT: metarole role for storing code attributes
 
+use Moose::Util::MetaRole;
+use Moose::Util qw/find_meta/;
+use Carp qw/croak/;
+
 use Moose::Role;
 
 use namespace::clean -except => 'meta';
@@ -20,6 +24,26 @@ around method_metaclass => sub {
         /],
         cache        => 1,
     )->name();
+};
+
+before 'apply' => sub {
+    my ($self, $thing) = @_;
+    if ($thing->isa('Moose::Meta::Class')) {
+        Moose::Util::MetaRole::apply_metaclass_roles(
+            for_class => $thing->name,
+            metaclass_roles => ['MooseX::MethodAttributes::Role::Meta::Class'],
+        );
+    }
+    elsif ($thing->isa('Moose::Meta::Role')) {
+        # No need to interfear with normal composition
+    }
+    else {
+        croak("Composing " . __PACKAGE__ . " onto instances is unsupported");
+    }
+    my $meta = find_meta($thing->name);
+    push @{ $meta->_method_attribute_list }, @{ $self->_method_attribute_list };
+    @{ $meta->_method_attribute_map }{ keys(%{ $self->_method_attribute_map }) }
+        = values %{ $self->_method_attribute_map };
 };
 
 1;
