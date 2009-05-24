ade1f3beaaded65a8aca45205d4a9fe2da8566b6
t0m <bobtfish@bobtfish.net>
2009-05-25 00:14:30 +0100

    Heh, I broke it if your class didn't already know about method attributes. That's what I get for not even reading the entire body of the method I'm patching, and/or not having small enough methods..

--- lib/MooseX/MethodAttributes/Role/Meta/Role.pm
+++ lib/MooseX/MethodAttributes/Role/Meta/Role.pm
@@ -57,17 +57,18 @@ around method_metaclass => sub {
 around 'apply' => sub {
     my ($orig, $self, $thing) = @_;
     if ($thing->isa('Moose::Meta::Class')) {
-        return if
+        unless (
            does_role($thing, 'MooseX::MethodAttributes::Role::Meta::Class')
         && does_role($thing->method_metaclass, 'MooseX::MethodAttributes::Role::Meta::Method')
-        && does_role($thing->wrapped_method_metaclass, 'MooseX::MethodAttributes::Role::Meta::Method::MaybeWrapped');
+        && does_role($thing->wrapped_method_metaclass, 'MooseX::MethodAttributes::Role::Meta::Method::MaybeWrapped')) {
         
-        Moose::Util::MetaRole::apply_metaclass_roles(
-            for_class => $thing->name,
-            metaclass_roles => ['MooseX::MethodAttributes::Role::Meta::Class'],
-            method_metaclass_roles => ['MooseX::MethodAttributes::Role::Meta::Method'],
-            wrapped_method_metaclass_roles => ['MooseX::MethodAttributes::Role::Meta::Method::MaybeWrapped'],
-        );
+            Moose::Util::MetaRole::apply_metaclass_roles(
+                for_class => $thing->name,
+                metaclass_roles => ['MooseX::MethodAttributes::Role::Meta::Class'],
+                method_metaclass_roles => ['MooseX::MethodAttributes::Role::Meta::Method'],
+                wrapped_method_metaclass_roles => ['MooseX::MethodAttributes::Role::Meta::Method::MaybeWrapped'],
+            );
+        }
     }
     elsif ($thing->isa('Moose::Meta::Role')) {
         Moose::Util::MetaRole::apply_metaclass_roles(
